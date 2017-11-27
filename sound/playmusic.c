f10ddaf3c046a2083cae0213ce2e9bd66876c577
Eunyoung <monaco9930@naver.com>
2017-11-27 02:56:29 -0800

    used system func

--- sound/playmusic.c
+++ sound/playmusic.c
@@ -10,16 +10,16 @@ void playmusic(int num)
    if(num == 1)
    {
      printf("Play music 1\n");
-    //system("aplay 1.wav");
+     system("play_num 1.wav");
    }
    else if(num == 2)
    {
      printf("Play music 2\n");
-     //system("aplay 2.wav");
+     system("play_num 2.wav");
    }
    else if(num == 3)
    {
      printf("Play music 3\n");
-     //system("aplay 3.wav");
+     system("play_num 3.wav");
    }
 }
