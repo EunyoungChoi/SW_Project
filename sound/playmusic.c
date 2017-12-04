9cd0ef227a344b5b12129b54d2be843504fe7640
Eunyoung <monaco9930@naver.com>
2017-12-04 02:32:45 -0800

    fixed aplay

--- sound/playmusic.c
+++ sound/playmusic.c
@@ -11,16 +11,16 @@ void playmusic(int num)
    if(num == 1)
    {
      printf("Play music 1\n");
-     system("play_num 1.wav");
+     system("aplay 1.wav");
    }
    else if(num == 2)
    {
      printf("Play music 2\n");
-     system("play_num 2.wav");
+     system("aplay 2.wav");
    }
    else if(num == 3)
    {
      printf("Play music 3\n");
-     system("play_num 3.wav");
+     system("aplay 3.wav");
    }
 }
